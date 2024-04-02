72bf30afa68b07589ccd324784c4fcba90aef3ad
lucrod23daw2 <441091.clot@fje.edu>
2024-04-02 19:35:44 +0200

    Visualitzar usuari implementat

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1> APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
 		<h2> DAW2 M08UF2 M08UF3 </h2>
-		<h3> Autor: lucrod2023daw2</h3>
+		<h3> Autor: lucrod23daw2</h3>
 		<h3> Correu: 441091.clot@fje.edu</h3>
 		<h3> Github: https://github.com/lucrod23daw2/daw2_m08uf23_projecte_rodriguez_lucia</h3>
 		<a href="http://zend-luroin.fjeclot.net/projecte/login.php">Inicia sessió</a>
--- menu.php
+++ menu.php
@@ -6,7 +6,10 @@
 	</head>
 	<body>
 		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
-		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
+		<a href="http://zend-luroin.fjeclot.net/projecte/formAfegir.php">Afegir usuari</a><br>
+		<a href="http://zend-luroin.fjeclot.net/projecte/formModificar.php">Modificar usuari</a><br>
+		<a href="http://zend-luroin.fjeclot.net/projecte/formEsborrar.php">Esborrar usuari</a><br>
+		<a href="http://zend-luroin.fjeclot.net/projecte/visualitzar.php">Visualitzar dades d'usuari</a><br>
 		<a href="http://zend-luroin.fjeclot.net/projecte/index.php">Torna a la pàgina inicial</a>
 	</body>
 </html>
